41d39d2e8233b797802a45238a57d4baf4c3752f
gdonscoi <donscoijr@gmail.com>
2017-11-20 15:19:40 -0200

    tirando fotos e salvando na pasta Pictures/FaceCaptcha

--- app/src/main/java/com/app/android/kotlin/facecaptcha/challenge/CameraManager.kt
+++ app/src/main/java/com/app/android/kotlin/facecaptcha/challenge/CameraManager.kt
@@ -1,45 +1,62 @@
 package com.app.android.kotlin.facecaptcha.challenge
 
+import android.util.Log
 import com.app.android.kotlin.facecaptcha.data.model.challenge.ChallengeResponse
+import java.util.*
+
 
 class CameraManager(private val challengeResponse: ChallengeResponse) {
+
+    private var count = 1
+    private val timer: Timer = Timer()
 
     fun start(totalTimeChallenges: Long, callback: CameraPresenter.ManagerCallback) {
         callback.onBeforeSend()
 
-        val delayTotalChallenges = (challengeResponse.totalTime - totalTimeChallenges)*1000
+        val delayTotalChallenges = (challengeResponse.totalTime - totalTimeChallenges) * 1000
+        var delayChallengeOverride: Long = 0
 
-        Thread({
-            Thread.sleep(delayTotalChallenges)
-            executeChallenges(callback)
-        }).start()
+        challengeResponse.challenges.forEach {
+            delayChallengeOverride += delayTotalChallenges
+            Log.i("delayChallengeOverride" , delayChallengeOverride.toString())
+            val delayChallenge = (it.tempoEmSegundos * 1000L)
 
-        startCounting(callback)
+            timer.schedule(executeChallenges(delayChallenge, it.mensagem, it.icone ?: "", callback), delayChallengeOverride, delayChallenge)
+        }
 
-        callback.onComplete()
+        timer.schedule(startCounting(callback), 0, 1000L)
     }
 
-    private fun executeChallenges(callback: CameraPresenter.ManagerCallback) {
-        challengeResponse.challenges.forEach {
-            callback.setMessage(it.mensagem)
-            callback.loadIcon(it.icone ?: "")
+    private fun executeChallenges(delayChallenge: Long, mensagem: String, icone: String, callback: CameraPresenter.ManagerCallback): TimerTask {
+        return object : TimerTask() {
+            override fun run() {
+                Log.i("executeChallenges" , mensagem)
 
-            val delayChallenge = it.tempoEmSegundos * 1000
-            val delayFrameMillis = challengeResponse.snapFrequenceInMillis.toLong()
-            val countIteration = (delayChallenge / challengeResponse.snapFrequenceInMillis.toLong()).toInt()
+                callback.setMessage(mensagem)
+                callback.loadIcon(icone)
 
-            callback.takePicture(countIteration, delayFrameMillis)
+                val delayFrameMillis = challengeResponse.snapFrequenceInMillis.toLong()
+                val countIteration = (delayChallenge / challengeResponse.snapFrequenceInMillis.toLong()).toInt()
 
-            Thread.sleep(delayChallenge.toLong())
+                callback.takePicture(countIteration, delayFrameMillis)
+                cancel()
+            }
         }
     }
 
-    private fun startCounting(callback: CameraPresenter.ManagerCallback) {
-        var count = 1
-        (count..challengeResponse.totalTime).forEach {
-            callback.setCount(count.toString())
-            Thread.sleep(1000)
-            count++
+    private fun startCounting(callback: CameraPresenter.ManagerCallback): TimerTask {
+        return object : TimerTask() {
+            override fun run() {
+                if (count > challengeResponse.totalTime) {
+                    callback.onComplete()
+                    timer.cancel()
+                    return
+                }
+
+                Log.i("startCounting" , count.toString())
+                callback.setCount(count.toString())
+                count++
+            }
         }
     }
 }
--- app/src/main/java/com/app/android/kotlin/facecaptcha/challenge/CameraPresenter.kt
+++ app/src/main/java/com/app/android/kotlin/facecaptcha/challenge/CameraPresenter.kt
@@ -1,34 +1,57 @@
 package com.app.android.kotlin.facecaptcha.challenge
 
+import android.hardware.Camera
+import android.os.Environment
+import android.util.Log
 import com.app.android.kotlin.facecaptcha.data.source.remote.MockChallengeRemoteDataSource
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class CameraPresenter(contract: CameraContract.View?, source: MockChallengeRemoteDataSource) {
 
     companion object {
         private var view: CameraContract.View? = null
         private var dataSource: MockChallengeRemoteDataSource? = null
+        private var photos: MutableCollection<ByteArray>? = null
     }
 
     init {
         view = contract
         dataSource = source
+        photos = ArrayList()
     }
+
+    var mPicture: Camera.PictureCallback? = null
 
     fun challenge(p: String) {
         Thread({
+            mPicture = Camera.PictureCallback { data, camera ->
+                photos?.add(data)
+                camera?.stopPreview()
+                camera?.startPreview()
+            }
+
             val challengeResponse = dataSource?.challenge(p, ChallengeCallback())
 
             var totalTimeChallenges = 0L
             challengeResponse?.challenges?.map { totalTimeChallenges += it.tempoEmSegundos }
 
+
             val manager = CameraManager(challengeResponse!!)
             manager.start(totalTimeChallenges, ManagerCallback())
+
         }).start()
     }
 
     fun destroy() {
         view = null
+        photos = null
+        mPicture = null
     }
+
 
     class ChallengeCallback {
         fun onBeforeSend() {
@@ -80,6 +103,39 @@ class CameraPresenter(contract: CameraContract.View?, source: MockChallengeRemot
             view?.finishCaptcha("Processando, aguarde...")
             view?.loadIcon("")
             view?.setCounter("")
+            Log.i("challenge fotos size: ", photos?.size.toString())
+
+            saveFiles()
+        }
+
+        private fun saveFiles() {
+            val sdDir = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val pictureFileDir = File(sdDir, "FaceCaptcha")
+
+            if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+                Log.d("Save file", "Muda na mão a permissão para salvar, teste soh para ver como fica as fotos")
+                return
+            }
+
+            photos?.forEach {
+                val dateFormat = SimpleDateFormat("yyyymmddhhmmssSSS", Locale.getDefault())
+                val date = dateFormat.format(Date())
+                val photoFile = "Picture_$date.jpg"
+
+                val filename = pictureFileDir.getPath() + File.separator + photoFile
+
+                val pictureFile = File(filename)
+
+                try {
+                    val fos = FileOutputStream(pictureFile)
+                    fos.write(it)
+                    fos.close()
+                } catch (error: Exception) {
+                    Log.d("Write", "File" + filename + "not saved: "
+                            + error.message)
+                }
+            }
         }
 
     }
